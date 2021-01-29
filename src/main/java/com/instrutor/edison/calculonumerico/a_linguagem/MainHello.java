4b322b4c9da47328fcb14334a03fa10729b14a08
spedison@gmail.com <spedison@gmail.com>
2021-01-29 20:18:39 -0300

    First Commit

---
+++ src/main/java/com/instrutor/edison/calculonumerico/a_linguagem/MainHello.java
@@ -0,0 +1,9 @@
+package com.instrutor.edison.calculonumerico.a_linguagem;
+
+public class MainHello {
+
+    public static void main(String[] args) {
+        System.out.println("Aqui estou");
+    }
+
+}
---
+++ src/main/java/com/instrutor/edison/calculonumerico/a_linguagem/MainMontaTabela.java
@@ -0,0 +1,16 @@
+package com.instrutor.edison.calculonumerico.a_linguagem;
+
+public class MainMontaTabela {
+
+    /*
+     * Como representar o número fracionário usando casas decimais flutuantes.(Escorregadias)
+     *   Segundo Norma IEEE 754
+     * */
+
+
+    public static void main(String[] args) {
+        for (int i = 1; i < 32 ; i++) {
+            System.out.printf("%d ; %28.26f\n", i , Math.pow(2, -i));
+        }
+    }
+}
